d4e14ae02c09bae1be217ada682d0e27d7773e32
maryam <129073630+MaryomaElsayed@users.noreply.github.com>
2024-07-27 05:57:11 +0300

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,15 +1,26 @@
-/* eslint-disable no-shadow */
+// /* eslint-disable no-shadow */
+// export default function taskBlock(trueOrFalse) {
+//   const task = false;
+//   const task2 = true;
+
+//   if (trueOrFalse) {
+//     // eslint-disable-next-line no-shadow
+//     const task = true;
+//     // eslint-disable-next-line no-shadow
+//     const task2 = false;
+//   }
+
+//   return [task, task2];
+// }
+// /* eslint-enable no-shadow */
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-next-line no-shadow
-    const task = true;
-    // eslint-disable-next-line no-shadow
-    const task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
 }
-/* eslint-enable no-shadow */
